25382e73ebe4ce6e775f9389083d2edaa4d1e3de
Andre Luis Pereira dos Santos <andre@andreconsultancy.eu.org>
2021-03-12 09:35:09 -0300

    improved error message in check of request fields of JSON

--- models/transactions.go
+++ models/transactions.go
@@ -97,7 +97,7 @@ func CreateTransaction(w http.ResponseWriter, r *http.Request) {
 			Transactions = append(Transactions, transaction)
 			json.NewEncoder(w).Encode("success")
 		} else {
-			json.NewEncoder(w).Encode("error: invalid Accoun ID or Operation Type ID")
+			json.NewEncoder(w).Encode("error: invalid Accoun ID, Operation Type ID or Amount field")
 		}
 	}
 
